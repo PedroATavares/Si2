3489b7609bc524134da0dd08ecb318db2c523b3b
GoncaloArnauth <goncaloarnauth@outlook.com>
2017-01-06 12:50:37 +0000

    alineas F, G e H feitas

--- App/App/Program.cs
+++ App/App/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System.Data.Common;
 
 namespace App
 {
@@ -10,6 +13,31 @@ namespace App
     {
         static void Main(string[] args)
         {
+            using (SqlConnection con = new SqlConnection())
+            {
+                try
+                {
+                    con.ConnectionString = @"Data Source=.;Initial Catalog=SI2_VLAB;Integrated Security=True";
+                    using (SqlCommand cmd = con.CreateCommand())
+                    {
+                        cmd.CommandText = "select value from DEMO";
+                        con.Open();
+
+                        using (SqlDataReader dr = cmd.ExecuteReader())
+                        {
+                            while (dr.Read())
+                                Console.Write(dr["value"] + "\n");
+                        }
+                    }
+
+                }
+                catch (DbException ex)
+                {
+                    Console.WriteLine("E R R O : " + ex.Message);
+                }
+
+            }
+        
         }
     }
 }
